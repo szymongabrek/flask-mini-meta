5fd17da69a6aa479d9393cdc6e47ce648d4bc258
Szymon Gabrek <szymongabrek@MacBook-Pro-Szymon.local>
2021-11-16 19:52:17 +0100

    flask meta lekcja2

--- app/__init__.py
+++ app/__init__.py
@@ -1,6 +1,8 @@
 import os
-from flask import Flask, render_template
+from flask import Flask, render_template,request, redirect
+from flask.helpers import url_for
 from . import db
+from werkzeug.security import generate_password_hash, check_password_hash
 
 app = Flask(__name__, instance_relative_config=True)
 
@@ -34,8 +36,36 @@ def login():
     return render_template("auth/login.html", title="Login")
 
 
-@app.route('/register')
+@app.route('/register', methods=["GET", "POST"])
 def register():
+    if request.method == "POST":
+        # Tutaj zaprogramuj dodanie uzytkownika do bazy
+        username = request.form["username"]
+        password = request.form["password"]
+
+        database = db.get_db()
+        error = None
+
+        if not username:
+            error = "Username is required"
+        elif not password:
+            error = "Dawaj hasło"
+        
+        if not error:
+            try:
+                database.execute(f"INSERT INTO user (username, password) VALUES (?, ?", (username, generate_password_hash(password)))
+
+                database.commit()
+            except database.IntegrityError:
+                error = f"User {username} is already registered"
+            else:
+                return redirect(url_for("login"))
+
+
+
+
+
+        return f"Tutaj kiedyś utworzymy Ci uzytkownika: {username}@{password}"
     return render_template("auth/register.html", title="Register")
 
 
